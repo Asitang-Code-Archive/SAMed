5bb65c9eeb2db5ed79907b61c3b9b896c86bd529
asitang <asitang@gmail.com>
2023-05-23 12:05:29 -0500

    Update sam_lora_image_encoder.py

--- sam_lora_image_encoder.py
+++ sam_lora_image_encoder.py
@@ -149,7 +149,7 @@ class LoRA_Sam(nn.Module):
 
         assert filename.endswith(".pt") or filename.endswith('.pth')
 
-        state_dict = torch.load(filename)
+        state_dict = torch.load(filename, map_location=torch.device('cpu'))
 
         for i, w_A_linear in enumerate(self.w_As):
             saved_key = f"w_a_{i:03d}"
